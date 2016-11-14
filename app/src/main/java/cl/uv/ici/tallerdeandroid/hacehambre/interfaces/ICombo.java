82d9287bc094d8e297669f2bf34f4353b3955b6f
Rodolfo Guíñez <elrodox@gmail.com>
2016-11-14 12:00:38 -0300

    First commit

---
+++ app/src/main/java/cl/uv/ici/tallerdeandroid/hacehambre/interfaces/ICombo.java
@@ -0,0 +1,9 @@
+package cl.uv.ici.tallerdeandroid.hacehambre.interfaces;
+
+/**
+ * Created by Rodolfo on 14/11/2016.
+ */
+public interface ICombo {
+    public int calculateCost();
+    public String getComboComponents();
+}
---
+++ app/src/main/java/cl/uv/ici/tallerdeandroid/hacehambre/model/Drink.java
@@ -0,0 +1,18 @@
+package cl.uv.ici.tallerdeandroid.hacehambre.model;
+
+import java.io.Serializable;
+
+public class Drink extends Food implements Serializable {
+
+    private String unit;
+
+
+    public Drink(String name, int cost, int weight, String type, String unit) {
+        super(name, cost, weight, type);
+        this.unit = unit;
+    }
+
+    public String getUnit(){
+        return unit;
+    }
+}
